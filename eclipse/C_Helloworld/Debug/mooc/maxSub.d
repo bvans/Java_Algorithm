defd292ee8e1aec0c35c4dd7d11771e41598e09d
vanvan <bvans2013@hotmail.com>
2015-04-26 11:56:55 +0800

    恢复了debug目录

---
+++ eclipse/C_Helloworld/Debug/mooc/maxSub.d
@@ -0,0 +1,1 @@
+mooc/maxSub.d: ../mooc/maxSub.c
---
+++ eclipse/C_Helloworld/Debug/mooc/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../mooc/maxSub.c 
+
+OBJS += \
+./mooc/maxSub.o 
+
+C_DEPS += \
+./mooc/maxSub.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+mooc/%.o: ../mooc/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cygwin C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
